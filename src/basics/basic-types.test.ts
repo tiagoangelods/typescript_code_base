e8d9fc641e2a05ef777e27bcb396d09f2c9c4ed8
Tiago Silva <tiagoangelods.dev@gmail.com>
2024-10-11 23:13:44 -0300

    test: tests array, object, and function types

--- src/basics/basic-types.test.ts
+++ src/basics/basic-types.test.ts
@@ -1,5 +1,12 @@
 import { describe, expect, test } from "vitest";
-import { arrayValue, booleanValue, numberValue, stringValue } from "./basic-types";
+import {
+  arrayValue,
+  booleanValue,
+  functionValue,
+  numberValue,
+  objectValue,
+  stringValue
+} from "./basic-types";
 describe('Testing Basic Types', () => {
   describe('Testing primitive types', () => {
     test("const stringValue should be a String type", () => {
@@ -22,11 +29,22 @@ describe('Testing Basic Types', () => {
     });
   });
   describe("Testing array, object and functions", () => {
-    test("const arrayValues should be an object type", () => {
+    test("const arrayValues should be an object type, remember array has object type in javascript/typescript", () => {
       expect(arrayValue).toBeTypeOf("object");
+      expect(Array.isArray(arrayValue)).toBe(true);
     });
     test("const arrayValue should be a length equals to 1", () => {
       expect(arrayValue).toHaveLength(1);
     });
+    test("const objectValue should be an object type", () => {
+      expect(objectValue).toBeTypeOf("object");
+    });
+    test("const objectValue to not have any property", () => {
+      expect(objectValue).toMatchObject({});
+    });
+    test("const functionValue should be an function type", () => {
+      expect(functionValue).toBeTypeOf("function");
+    });
+    
   });
 });
--- src/basics/basic-types.ts
+++ src/basics/basic-types.ts
@@ -4,3 +4,5 @@ export const numberValue: number = 8;
 export const booleanValue: boolean = true;
 // array, object and functions
 export const arrayValue: Array<any> = new Array(1);
+export const objectValue: Object = Object.create({});
+export const functionValue: Function = () => {};
